5888082d64fb3480ea4c6ab3fe13065d51e532c8
ENRICO MEIRA DE PAULA <210334@lti.fatecso.edu.br>
2023-09-01 14:46:25 -0300

    Correção do Index para php

--- src/banco/conexao.php
+++ src/banco/conexao.php
@@ -8,10 +8,10 @@
 	$banco = mysqli_select_db($conexao, $banco);
 	$conexao -> set_charset("utf8");
 	
-	if ($conexao->connect_errono()) 
-	{
-		die("Falha de conexão com banco de dados: ". 
-		mysqli_connect_error() . " ( " . mysqli_connect_errorno(). " )"
-		);
-	}
+	// if ($conexao->connect_errono()) 
+	// {
+	// 	die("Falha de conexão com banco de dados: ". 
+	// 	mysqli_connect_error() . " ( " . mysqli_connect_errorno(). " )"
+	// 	);
+	// }
 ?>
