491be088adc73d34ffe5d697e59629626c7b0ca5
ENRICO MEIRA DE PAULA <210334@lti.fatecso.edu.br>
2023-09-01 14:48:40 -0300

    Correção do Index para php

---
+++ index.php
@@ -0,0 +1,85 @@
+<?php 
+    include_once("src/html/header.php");
+?>
+        <!-- <img src="/src/imagens/background.jpg" alt="" class="bg-image"> -->
+        <main class="main-container bg">
+            <div class=introduction>
+                <div class="name-container">
+                        <h2>
+                            Fernanda
+                            <span>Meira</span>
+                        </h2>
+                </div>
+                <div class="saber">
+                    <a href="">
+
+                    </a>
+                    <a href="">
+                        
+                    </a>
+                </div>
+            </div>
+
+            <div class="sobre">
+                <div class="sobre-container">
+                    <div class="sobre img">
+                        <div class="init-anim">
+                            <img src="">
+                        </div>
+                    </div>
+                    <div class="sobre-text">
+                        <div class="init-anim">
+                            <h3>
+                                titulo
+                            </h3>
+                            <p>
+                                texto
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="linha">
+                <div class="linha-container">
+                    <div class="init-anim">
+                        <h2>
+                            principais tratamentos
+                        </h2>
+                        <div class="linha-title">
+                            ::before
+                            <img src="imagem">
+                            ::after
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!-- as tags br são só para visualização no site atual e devem ser retiradas na hora de começar a usar o CSS -->
+            <br>
+            <br>
+
+            <div class="init-anim">
+                <div class="tratamentos">
+                    <div class="tratamentos-container">
+                        <button class="button-prev">botão prev</button>
+                        <div>
+                            tratamentos<!-- aqui vai ter todas as divs dos tratamentos, como são muitas e depende da quantidade de tratamentos achei melhor deixar vazio por enquanto. -->
+                        </div>
+                        <button class="button-next">botão next</button>
+                    </div>
+                </div>
+            </div>
+
+            <br>
+            <br>
+
+            <div class="iframe">  
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.7135749635636!2d-47.462281188996116!3d-23.506824078749005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c58b11344dc07d%3A0x1750c849b8933f8e!2sInstituto%20F.%20Meira%20Est%C3%A9tica%20Avan%C3%A7ada!5e0!3m2!1spt-BR!2sbr!4v1683595038516!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </main>
+
+<?php 
+    include_once("src/html/footer.php");
+?>
+
