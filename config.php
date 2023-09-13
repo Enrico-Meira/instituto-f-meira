086d3abd66fdd5ace12e0fbff34397534bfca423
DAVI DA COSTA RUIZ <210338@lti.fatecso.edu.br>
2023-09-13 18:02:55 -0300

    att dos estilos

--- config.php
+++ config.php
@@ -9,7 +9,7 @@
 		define('abspath', dirname(__FILE__) . '/');
 	
 	if (!defined("baseurl"))
-		define("baseurl", "/tcc_enrico");
+		define("baseurl", "/instituto-f-meira");
 
 	if (!defined('dbpath'))
 		define('dbpath', abspath . "src/inc/funcoes.php");
