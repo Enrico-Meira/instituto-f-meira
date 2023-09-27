d86d8ca0996a267e8d3f3865df678bfa12c64255
DAVI DA COSTA RUIZ <210338@lti.fatecso.edu.br>
2023-09-27 17:42:22 -0300

    ajustes no style

--- index.php
+++ index.php
@@ -20,11 +20,38 @@
                 </div>
             </div>
 
-            <div class="sobre">
-                <div class="sobre-container">
-                    <div class="sobre img">
+            <div class="info">
+                <div class="info-container">
+                    <div class="row">
                         <div class="init-anim">
-                            <img src="">
+                            <a href="">
+                                <img src="" alt="">
+                            </a>
+                        </div>
+                        <div class="init-anim">
+                            <h3></h3>
+                            <p>
+                                Buuu 👻 Oi migos, tudo bom com vocês?? 
+                                Deixa eu te contar um capítulo de uma historinha. 
+                                Quando terminei minha Especialização em Biomedicina Estética, 
+                                foquei em aprimorar as técnicas de procedimentos injetáveis que tinha aprendido. 
+                                Realizei diversos cursos na área, com profissionais excelentes, 
+                                em São Paulo, Belo Horizonte, Rio Grande do Sul.
+                            </p>
+                            <p>
+                                Em 2022, tive a oportunidade de realizar um curso em cadáver fresco nos Estados Unidos, 
+                                e eu garanto, isso elevou muito minha curva de aprendizagem. 
+                                Hoje atendo meus pacientes no Instituto F. Meira 
+                                (aquela "salinha 93", como eu gosto carinhosamente de chamar 😜). 
+                                Eu te contei que eu amo aprender né? E claro que eu também amo ensinar.... 
+                                Pode perguntar pros meus 250 alunos ou mais da UNICORP.
+                            </p>
+                            <p>
+                                Bom migos esse é só um pedacinho das coisas legais que eu queria compartilhar com vocês hoje,
+                                mas não sai daqui que eu tô sempre aprontando algo...!! 
+                                E se você me perguntar se o caminho foi fácil, eu te digo que foi a vida, 
+                                ela é fácil todo dia ?? Beijos e fui ;)
+                            </p>
                         </div>
                     </div>
                     <div class="sobre-text">
