e173a759766bce3317734d6b984bd1339d3eb60a
ENRICO MEIRA DE PAULA <210334@lti.fatecso.edu.br>
2023-10-27 14:58:20 -0300

    alterações no procedimentos e adições ao footer

--- src/procedimentos/index.php
+++ src/procedimentos/index.php
@@ -6,39 +6,35 @@
     <main>
         <div class="trat-title">
             <div class="trat-container">
-                <div class="init-anim">
                     <h2>
-                        principais tratamentos
+                        Principais Tratamentos
                     </h2>
                     <div class="linha-title">
-                        <img src="<?php echo baseurl; ?>/src/imagens/logo.png">
+                        <img class="linha-img" src="<?php echo baseurl; ?>/src/imagens/logo.png">
                     </div>
-                </div>
             </div>
         </div>
+        
+                                       
+        
 
-        <div class="foto init-anim">
-            <div class="tratamento-container">
-                <div class="autoplay">
-                    <div class="slick-list">
-                        <div class="slick-track">
-                            <div class="slick-slide">
-                                <div>
-                                    <div><!-- estilo vai ser definido na hora de animar com css -->
-                                        <a href="./protocoll.php">
-                                            <img src="" alt="">
-                                            <h2>Protocoll</h2>
-                                            <p>Esse tratamento tem o objetivo de reestruturar a pele, através da aplicação de toxinas que estimulam a produção de colágeno.</p>
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
 
+
+
+
+
+
+
+
+
+
+
+
+        <!-- <a href="./protocoll.php">
+        <img class="protocoll-img"src="<?php echo baseurl; ?>/src/imagens/logo.png" alt="">
+        <h2>Protocoll</h2>
+        <p>Esse tratamento tem o objetivo de reestruturar a pele, através da aplicação de toxinas que estimulam a produção de colágeno.</p>
+        </a> -->
     </main>
 <?php
     require_once(footer_template);
