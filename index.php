2aee791489cac4c79bc91bbc73d91d2a81f10311
Enrico Meira <enrico.depaulameira@gmail.com>
2023-11-12 05:05:05 -0300

    página tratamentos completa, menu slider automático iniciado no index

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
             <div class="pt"> 
                 <div class="pt-container">
                         <h2>
-                            principais tratamentos
+                            Principais procedimentos
                         </h2>
                         <div class="linha-title">
                             ::before
@@ -34,16 +34,42 @@
                 </div>
             </div>
 
-    
-            <div class="imgs-container">
-                <div class="autoplay">
-                    <button class="button-prev">botão prev</button>
-                    <div>
-                        <!-- AQUI VIRÁ O MENU DE SLIDE AUTOMÁTICO -->
+            <div class="slider">
+                <div class="list">
+                    <div class="item trat-container">
+                        <img src="<?php echo baseurl; ?>/src/imagens/200.png" alt="">
+                        <h2>Preenchimento facial</h2>
+                        <p></p>
                     </div>
-                    <button class="button-next">botão next</button>
+                    <div class="item trat-container">
+                        <img src="<?php echo baseurl; ?>/src/imagens/200.png" alt="">
+                        <h2>Preenchimento facial</h2>
+                        <p></p>
+                    </div>
+                    <div class="item trat-container">
+                        <img src="<?php echo baseurl; ?>/src/imagens/200.png" alt="">
+                        <h2>Preenchimento facial</h2>
+                        <p></p>
+                    </div>
+                    <div class="item trat-container">
+                        <img src="<?php echo baseurl; ?>/src/imagens/200.png" alt="">
+                        <h2>Preenchimento facial</h2>
+                        <p></p>
+                    </div>
+                    <div class="item trat-container">
+                        <img src="<?php echo baseurl; ?>/src/imagens/200.png" alt="">
+                        <h2>Preenchimento facial</h2>
+                        <p></p>
+                    </div>
                 </div>
             </div>
+
+            <div class="buttons">
+                <button id="prev"><</button>
+                <button id="next">></button>
+            </div>
+            
+
 
             <br>
             <br>
@@ -51,6 +77,9 @@
             <div class="iframe">  
                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3658.7135749635636!2d-47.462281188996116!3d-23.506824078749005!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94c58b11344dc07d%3A0x1750c849b8933f8e!2sInstituto%20F.%20Meira%20Est%C3%A9tica%20Avan%C3%A7ada!5e0!3m2!1spt-BR!2sbr!4v1683595038516!5m2!1spt-BR!2sbr" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
+
+
+            <script src="<?php echo baseurl; ?>/src/js/slider.js"></script>
         </main>
 
 <?php 
--- src/procedimentos/index.php
+++ src/procedimentos/index.php
@@ -4,10 +4,10 @@
     include(header_template);   
 ?>
     <main>
-        <div class="trat-title">
-            <div class="trat-container">
+        <div class="trat-title-container">
+            <div class="trat-title-container">
                     <h2>
-                        Principais Tratamentos
+                        Procedimentos e tratamentos
                     </h2>
                     <div class="linha-title">
                         <img class="linha-img" src="<?php echo baseurl; ?>/src/imagens/logo.png">
@@ -16,7 +16,217 @@
         </div>
         
                                        
-        
+        <div class="general-container">
+            <div class="trats-container">
+                <div class="trat-container">
+                    <a href="">
+                        <img class="linha-img" src="<?php echo baseurl; ?>/src/imagens/200.png">
+                        <h2>Toxina botulínica - botox</h2>
+                        <p> O Botox é uma técnica que utiliza a toxina botulínica para suavizar rugas e linhas de expressão, proporcionando uma aparência mais jovem e relaxada.</p>
+                    </a>
+                </div>
+
+                <div class="trat-container">
+                    <a href="">
+                        <img class="linha-img" src="<?php echo baseurl; ?>/src/imagens/200.png">
+                        <h2>Rinomodelação</h2>
+                        <p>É um procedimento não cirúrgico que utiliza preenchimentos dérmicos para melhorar a aparência do nariz, corrigindo irregularidades no formato ou tamanho.</p>
+                    </a>
+                </div>
+
+                <div class="trat-container">
+                    <a href="">
+                        <img class="linha-img" src="<?php echo baseurl; ?>/src/imagens/200.png">
+                        <h2>Preenchimento labial</h2>
+                        <p>É um procedimento que utiliza substâncias para aumentar o volume e melhorar o contorno dos lábios, proporcionando uma aparência mais cheia e definida.</p>
+                    </a>
+                </div>
+
+                <div class="trat-container">
+                    <a href="">
+                        <img class="linha-img" src="<?php echo baseurl; ?>/src/imagens/200.png">
+                        <h2>Bioestimuladores de colágeno</h2>
+                        <p>São substâncias injetáveis que estimulam a produção de colágeno na pele. São utilizados para tratar rugas, flacidez e melhorar a textura da pele</p>
+                    </a>
+                </div>
+
+                <div class="trat-container">
+                    <a href="">
+                        <img class="linha-img" src="<?php echo baseurl; ?>/src/imagens/200.png">
+                        <h2>Fios de PDO</h2>
+                        <p> São fios absorvíveis que são inseridos na pele para promover um efeito de lifting facial, melhorando a firmeza e a sustentação da pele.</p>
+                    </a>
+                </div>
+            </div>
+
+            <div class="trats-container">
+                <div class="trat-container">
+                    <a href="">
+                        <img class="linha-img" src="<?php echo baseurl; ?>/src/imagens/200.png">
+                        <h2>Microagulhamento</h2>
+                        <p></p>
+                    </a>
+                </div>
+
+                <div class="trat-container">
+                    <a href="">
+                        <img class="linha-img" src="<?php echo baseurl; ?>/src/imagens/200.png">
+                        <h2>Peeling químico</h2>
+                        <p></p>
+                    </a>
+                </div>
+
+                <div class="trat-container">
+                    <a href="">
+                        <img class="linha-img" src="<?php echo baseurl; ?>/src/imagens/200.png">
+                        <h2>Skinbooster</h2>
+                        <p></p>
+                    </a>
+                </div>
+
+                <div class="trat-container">
+                    <a href="">
+                        <img class="linha-img" src="<?php echo baseurl; ?>/src/imagens/200.png">
+                        <h2>Intradermoterapia</h2>
+                        <p></p>
+                    </a>
+                </div>
+
+                <div class="trat-container">
+                    <a href="">
+                        <img class="linha-img" src="<?php echo baseurl; ?>/src/imagens/200.png">
+                        <h2>Microtox</h2>
+                        <p></p>
+                    </a>
+                </div>
+            </div>
+
+            <div class="trats-container">
+                <div class="trat-container">
+                    <a href="">
+                        <img class="linha-img" src="<?php echo baseurl; ?>/src/imagens/200.png">
+                        <h2>Cicatriz de acne</h2>
+                        <p></p>
+                    </a>
+                </div>
+
+                <div class="trat-container">
+                    <a href="">
+                        <img class="linha-img" src="<?php echo baseurl; ?>/src/imagens/200.png">
+                        <h2>Melasma/manchas</h2>
+                        <p></p>
+                    </a>
+                </div>
+
+                <div class="trat-container">
+                    <a href="">
+                        <img class="linha-img" src="<?php echo baseurl; ?>/src/imagens/200.png">
+                        <h2>Hidratação facial</h2>
+                        <p></p>
+                    </a>
+                </div>
+
+                <div class="trat-container">
+                    <a href="">
+                        <img class="linha-img" src="<?php echo baseurl; ?>/src/imagens/200.png">
+                        <h2>Lipo enzimática de papada</h2>
+                        <p></p>
+                    </a>
+                </div>
+
+                <div class="trat-container">
+                    <a href="">
+                        <img class="linha-img" src="<?php echo baseurl; ?>/src/imagens/200.png">
+                        <h2>Tratamentos para pescoço</h2>
+                        <p></p>
+                    </a>
+                </div>
+            </div>
+
+            <div class="trats-container">
+                <div class="trat-container">
+                    <a href="">
+                        <img class="linha-img" src="<?php echo baseurl; ?>/src/imagens/200.png">
+                        <h2>Enzimas</h2>
+                        <p></p>
+                    </a>
+                </div>
+
+                <div class="trat-container">
+                    <a href="">
+                        <img class="linha-img" src="<?php echo baseurl; ?>/src/imagens/200.png">
+                        <h2>Trat. gordura localizada</h2>
+                        <p></p>
+                    </a>
+                </div>
+
+                <div class="trat-container">
+                    <a href="">
+                        <img class="linha-img" src="<?php echo baseurl; ?>/src/imagens/200.png">
+                        <h2>Hidrolipoclasia</h2>
+                        <p></p>
+                    </a>
+                </div>
+
+                <div class="trat-container">
+                    <a href="">
+                        <img class="linha-img" src="<?php echo baseurl; ?>/src/imagens/200.png">
+                        <h2>Flacidez corporal</h2>
+                        <p></p>
+                    </a>
+                </div>
+
+                <div class="trat-container">
+                    <a href="">
+                        <img class="linha-img" src="<?php echo baseurl; ?>/src/imagens/200.png">
+                        <h2>Limpeza de pele</h2>
+                        <p></p>
+                    </a>
+                </div>
+            </div>
+             
+            <div class="trats-container">
+                <div class="trat-container">
+                    <a href="">
+                        <img class="linha-img" src="<?php echo baseurl; ?>/src/imagens/200.png">
+                        <h2>Drenagem linfática</h2>
+                        <p></p>
+                    </a>
+                </div>
+
+                <div class="trat-container">
+                    <a href="">
+                        <img class="linha-img" src="<?php echo baseurl; ?>/src/imagens/200.png">
+                        <h2>Preenchimento facial</h2>
+                        <p></p>
+                    </a>
+                </div>
+
+                <div class="trat-container">
+                    <a href="">
+                        <img class="linha-img" src="<?php echo baseurl; ?>/src/imagens/200.png">
+                        <h2>Lipo enzimática de papada</h2>
+                        <p></p>
+                    </a>
+                </div>
+
+                <div class="trat-container">
+                    <a href="">
+                        <img class="linha-img" src="<?php echo baseurl; ?>/src/imagens/200.png">
+                        <h2>Protocol. para emagrecimento</h2>
+                        <p></p>
+                    </a>
+                </div>
+
+                <div class="trat-container">
+                    <a href="">
+                        <img class="linha-img" src="<?php echo baseurl; ?>/src/imagens/200.png">
+                        <h2>Definição abdominal</h2>
+                        <p></p>
+                    </a>
+                </div>
+            </div>
+        </div>
 
 
 
