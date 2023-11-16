c7efb3ac6dc47ba89affdb4e6d8f21d588b88c2b
Enrico Meira <enrico.depaulameira@gmail.com>
2023-11-16 03:06:56 -0300

    footer finalizado e apresentação dos painéis autoplay do index

--- src/inc/footer.php
+++ src/inc/footer.php
@@ -1,23 +1,42 @@
-        <footer>
-            <h3>Fernanda Meira</h3>
-            <div class="links-sociais">
-                <a href="">
-                    <i>instagram</i>
-                </a>
-                <br>
-                <a href="">
-                    <i>outra rede social</i>
-                </a>
-            </div>
-            <div class="creditos">
-                <div class="logo-footer">
-                        <img src="">
-                </div>
-                <div class="copyright">
-                    @ copytexto
-                </div>
-                <div class="credits">
-                    texto credito
+        <footer class="footer">
+            <div class="footer-container">
+                <div class="footer-row">
+                    <div class="footer-col">
+                        <h4>company</h4>
+                        <ul>
+                            <li><a href="">about us</a></li>
+                            <li><a href="">our services</a></li>
+                            <li><a href="">privacy policy</a></li>
+                            <li><a href="">affiliate program</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-col">
+                        <h4>get help</h4>
+                        <ul>
+                            <li><a href="">FAQ</a></li>
+                            <li><a href="">shipping</a></li>
+                            <li><a href="">returns</a></li>
+                            <li><a href="">payment options</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-col">
+                        <h4>online shop</h4>
+                        <ul>
+                            <li><a href="">watch</a></li>
+                            <li><a href="">bag</a></li>
+                            <li><a href="">shoes</a></li>
+                            <li><a href="">dress</a></li>
+                        </ul>
+                    </div>
+                    <div class="footer-col">
+                        <h4>follow us</h4>
+                        <div class="social-links">
+                            <a href=""><i class="fab fa-facebook-f"></i></a>
+                            <a href=""><i class="fab fa-twitter"></i></a>
+                            <a href=""><i class="fab fa-instagram"></i></a>
+                            <a href=""><i class="fab fa-linkedin-in"></i></a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </footer>
